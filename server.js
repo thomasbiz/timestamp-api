15aeabf28f097ce9d0b27db8c33750d8d846882e
Thomas Berglund <thomas@thomasb.biz>
2016-11-08 14:39:06 +0000

    Added static help page.

--- server.js
+++ server.js
@@ -2,14 +2,26 @@ var express = require('express');
 
 var app = express()
 
+app.get('/', function(req, res) {
+  res.sendFile('index.html', { root: __dirname + '/public/' });
+});
+
 app.get('/:date', function (req, res) {
-  var date = new Date(req.params.date);
+  var input = req.params.date;
+  var date;
   var obj = {};
+  
+  if (Number.isInteger(+input)) {
+    date = new Date(+input);
+  } else {
+    date = new Date(req.params.date);
+  }
+
   if (isNaN(date.getTime())) {
     obj.unix = null;
     obj.natural = null;
   } else {
-    obj.unix = date.getTime() / 1000;
+    obj.unix = date.getTime();
     obj.natural = date.toLocaleString("en-us", { month: "long", year: "numeric", day: "numeric" });
   }
 
