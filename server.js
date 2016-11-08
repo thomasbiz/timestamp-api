3440b9f5a941d5ddb0b32ebf5a886842acdf962c
Thomas Berglund <thomas@thomasb.biz>
2016-11-08 14:17:26 +0000

    done!

--- server.js
+++ server.js
@@ -2,8 +2,18 @@ var express = require('express');
 
 var app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.get('/:date', function (req, res) {
+  var date = new Date(req.params.date);
+  var obj = {};
+  if (isNaN(date.getTime())) {
+    obj.unix = null;
+    obj.natural = null;
+  } else {
+    obj.unix = date.getTime() / 1000;
+    obj.natural = date.toLocaleString("en-us", { month: "long", year: "numeric", day: "numeric" });
+  }
+
+  res.send(JSON.stringify(obj));
 })
 
 app.listen(process.env.PORT, function () {
